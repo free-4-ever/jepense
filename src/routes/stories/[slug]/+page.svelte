9edc7492a9bfbaf7cf28344fc3819374eec8165c
Ali Bakhshandeh <yakari1991@gmail.com>
2023-04-03 17:52:40 +0330

    loading skeletons in place for stories while editorjs is setup

--- src/routes/stories/[slug]/+page.svelte
+++ src/routes/stories/[slug]/+page.svelte
@@ -10,38 +10,102 @@
 	import Code from '@editorjs/code';
 	import Link from '@editorjs/link';
 	import Embed from '@editorjs/embed';
-	
-	export let data: PageServerData;
-	
-	onMount(async () => {
-		if (browser) {
-			new EditorJS({
-				readOnly: true,
-				// Other configuration properties
-				tools: {
-					image: ImageTool,
-					header: Header,
-					list: List,
-					delimiter: Delimiter,
-					code: Code,
-					link: Link,
-					embed: Embed
-				},
 
-				/**
-				 * Initial Editor data
-				 */
-				data: JSON.parse(data.post!.content!),
-				holder: 'editorjs'
-			});
-		}
+	export let data: PageServerData;
+
+	let ready = false;
+
+	onMount(async () => {
+		new EditorJS({
+			readOnly: true,
+			// Other configuration properties
+			tools: {
+				image: ImageTool,
+				header: Header,
+				list: List,
+				delimiter: Delimiter,
+				code: Code,
+				link: Link,
+				embed: Embed
+			},
+
+			/**
+			 * Initial Editor data
+			 */
+			data: JSON.parse(data.post!.content!),
+			holder: 'editorjs',
+			onReady: () => {
+				// alert()
+				ready = true;
+			}
+		});
 	});
 </script>
 
-{#if browser}
+<div class="row items-x-center">
 	<div class="col-m-9 col-s-10 col-l-8 text-center f-lll">
-		<div id="editorjs" />
+		<div id="editorjs">
+			{#if !ready}
+				<div class="skeleton-loader wrapper">
+					<div class="header skeleton-loader" />
+					<div class="line skeleton-loader" />
+					<div class="line skeleton-loader" />
+					<div class="line skeleton-loader" />
+					<div class="line skeleton-loader" />
+					<div class="line skeleton-loader" />
+					<div class="box skeleton-loader" />
+				</div>
+			{/if}
+		</div>
 	</div>
-{:else}
-	<h1>W8</h1>
-{/if}
+</div>
+
+<style>
+	.header {
+		height: 40px;
+		margin: 2rem 10rem;
+	}
+	.skeleton-loader:empty {
+		display: block;
+		background: linear-gradient(
+				to right,
+				rgba(255, 255, 255, 0),
+				rgba(255, 255, 255, 0.5) 50%,
+				rgba(255, 255, 255, 0) 80%
+			),
+			rgb(235, 233, 233);
+		background-repeat: repeat-y;
+		background-size: 50px 500px;
+		background-position: 0 0;
+		animation: shine 1s infinite;
+	}
+
+	.line {
+		height: 15px;
+		margin: 0.5rem 6rem;
+	}
+
+	.box {
+		height: 300px;
+		margin: 1rem 6rem;
+	}
+
+	@keyframes shine {
+		to {
+			background-position: 100% 0;
+		}
+	}
+
+	@media only screen and (max-width: 600px) {
+		.header {
+			margin: 1rem 5rem;
+		}
+
+		.line {
+			margin: 0.5rem 3rem;
+		}
+		.box {
+			margin: 1rem 3rem;
+		}
+	}
+</style>
